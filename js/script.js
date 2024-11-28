37492d7e3f007f424f69dbb79e9527001293d8c4
sohanshrestha12 <sohanshrestha40@gmail.com>
2024-11-28 21:46:03 +0545

    Feat: infinite scrolling

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ window.addEventListener("load", () => {
   const slider = document.querySelector(".card-slider");
   const cards = Array.from(document.querySelectorAll(".card"));
 
+  const clonedCards = cards.map((card) => card.cloneNode(true));
+
+  clonedCards.forEach((card) => slider.appendChild(card));
+
+  clonedCards.forEach((card) =>
+    slider.insertBefore(card.cloneNode(true), cards[0])
+  );
+  const allCards = Array.from(slider.querySelectorAll(".card"));
+
+  slider.scrollTop = cards[0].clientHeight * cards.length;
+
   let isDown = false;
   let startY;
   let scrollTop;
@@ -23,19 +34,30 @@ window.addEventListener("load", () => {
   slider.addEventListener("mousemove", (e) => {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageY - slider.offsetTop;
-    const walk = x - startY;
+    const y = e.pageY - slider.offsetTop;
+    const walk = y - startY;
     slider.scrollTop = scrollTop - walk;
   });
 
   slider.addEventListener("scroll", () => {
+    const sliderHeight = slider.scrollHeight;
+    const cardHeight = allCards[0].clientHeight;
+    const visibleHeight = slider.clientHeight;
+
+    if (slider.scrollTop <= 0) {
+      slider.scrollTop =
+        sliderHeight - visibleHeight - cardHeight * cards.length;
+    } else if (slider.scrollTop + visibleHeight >= sliderHeight) {
+      slider.scrollTop = cardHeight * cards.length;
+    }
+
     const sliderMiddle =
       slider.getBoundingClientRect().top + slider.clientHeight / 2;
 
     let closestCard = null;
     let closestDistance = Number.POSITIVE_INFINITY;
 
-    cards.forEach((card) => {
+    allCards.forEach((card) => {
       const cardMiddle =
         card.getBoundingClientRect().top + card.clientHeight / 2;
       const distance = Math.abs(cardMiddle - sliderMiddle);
@@ -46,7 +68,7 @@ window.addEventListener("load", () => {
       }
     });
 
-    cards.forEach((card) => card.classList.remove("active"));
+    allCards.forEach((card) => card.classList.remove("active"));
     if (closestCard) {
       closestCard.classList.add("active");
 
